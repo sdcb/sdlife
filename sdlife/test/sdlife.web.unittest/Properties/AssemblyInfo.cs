bfb55c2173c66d89c490e06550659600b09318bf
flysha <flysha@live.com>
2016-05-14 10:04:08 +0800

    add unittest

---
+++ sdlife/test/sdlife.web.unittest/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("sdlife.web.unittest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sdlife.web.unittest")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a832c4c2-4e6e-429a-8eea-d550ef7ce110")]
---
+++ sdlife/test/sdlife.web.unittest/Services/PinYinService_Test.cs
@@ -0,0 +1,56 @@
+﻿using sdlife.web.Services.Implements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace sdlife.web.unittest.Services
+{
+    public class PinYinService_Test
+    {
+        private readonly PinYinConverter pinYin;
+
+        public PinYinService_Test()
+        {
+            pinYin = new PinYinConverter();
+        }
+
+        [Theory]
+        [InlineData('我', "Wo")]
+        [InlineData('嬴', "Ying")]
+        [InlineData('长', "Chang")]
+        [InlineData('\0', "\0")]
+        [InlineData('-', "-")]
+        [InlineData('A', "A")]
+        public void GetCharPinYin(char ch, string expectedPinYin)
+        {
+            var actualPinYin = pinYin.GetCharPinYin(ch);
+            Assert.Equal(expectedPinYin, actualPinYin);
+        }
+
+        [Theory]
+        [InlineData("我是中国人", "WoShiZhongGuoRen")]
+        [InlineData("早餐", "ZaoCan")]
+        [InlineData("长沙", "ChangSha")]
+        [InlineData(null, null)]
+        [InlineData("\0A.领导", "\0A.LingDao")]
+        public void GetStringPinYin(string word, string expectedPinYin)
+        {
+            var actualPinYin = pinYin.GetStringPinYin(word);
+            Assert.Equal(expectedPinYin, actualPinYin);
+        }
+
+        [Theory]
+        [InlineData("我是中国人", "WSZGR")]
+        [InlineData("早餐", "ZC")]
+        [InlineData("长沙", "CS")]
+        [InlineData(null, null)]
+        [InlineData("\0A.领导", "\0A.LD")]
+        public void GetStringCapitalPinYin(string word, string expectedPinYin)
+        {
+            var actualPinYin = pinYin.GetStringCapitalPinYin(word);
+            Assert.Equal(expectedPinYin, actualPinYin);
+        }
+    }
+}
