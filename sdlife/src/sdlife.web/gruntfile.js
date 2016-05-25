291d8349f456071924c1078b75dd79c9ac534159
Flysha <sdflysha@qq.com>
2016-05-25 18:18:11 +0800

    add angular template bundler

---
+++ sdlife/src/sdlife.web/gruntfile.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"), 
+        ngtemplates: {
+            myapp: {
+                options: {
+                    module: 'sdlife',
+                },
+                src: 'wwwroot/app/*/*.html',
+                dest: 'wwwroot/app/app.tpl.min.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-angular-templates");
+    grunt.registerTask("default", ["ngtemplates"])
+}
