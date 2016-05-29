41ed2b1701184b4a994d4090b0c43d949e907ffd
flysha <flysha@live.com>
2016-05-29 20:35:20 +0800

    add basic menu

---
+++ sdlife/src/sdlife.web/wwwroot/app/common/menu/menu.ts
@@ -0,0 +1,12 @@
+﻿namespace sdlife {
+    let app = angular.module(consts.moduleName);
+
+    class SdlifeMenuComponent {
+    }
+
+    app.component("sdlifeMenu", {
+        templateUrl: "/app/common/menu/menu.html", 
+        controller: SdlifeMenuComponent, 
+        controllerAs: "vm", 
+    });
+}
