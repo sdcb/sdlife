b3790058ce369037b495032fcafe2ea5080d5bd9
Flysha <sdflysha@qq.com>
2016-06-02 17:43:08 +0800

    research Csrf drop AntiForgeryToken

--- sdlife/src/sdlife.web/Controllers/AccountController.cs
+++ sdlife/src/sdlife.web/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using sdlife.web.Dtos;
 using Microsoft.AspNetCore.Identity;
 using sdlife.web.Models;
 using sdlife.web.Managers;
+using Microsoft.AspNetCore.Antiforgery;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,15 +17,18 @@ namespace sdlife.web.Controllers
     {
         private readonly SignInManager<User> _signInManager;
         private readonly SdlifeUserManager _userManager;
+        private readonly IAntiforgery _antiforgery;
 
         public AccountController(
-            SdlifeUserManager userManager, 
-            SignInManager<User> signInManager)
+            SdlifeUserManager userManager,
+            SignInManager<User> signInManager,
+            IAntiforgery antiforgery)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _antiforgery = antiforgery;
         }
-        
+
         public async Task<IActionResult> Login([FromBody]LoginDto loginDto)
         {
             var user = await _userManager.FindByNameOrEmailAsync(loginDto.UserName);
@@ -51,8 +55,8 @@ namespace sdlife.web.Controllers
         public async Task<IActionResult> ChangePassword([FromBody]ChangePasswordDto changePasswordDto)
         {
             var user = await _userManager.GetUserAsync(User);
-            var result = await _userManager.ChangePasswordAsync(user, 
-                changePasswordDto.CurrentPassword, 
+            var result = await _userManager.ChangePasswordAsync(user,
+                changePasswordDto.CurrentPassword,
                 changePasswordDto.NewPassword);
             return FromIdentityResult(result);
         }
@@ -62,6 +66,16 @@ namespace sdlife.web.Controllers
             var user = (User)createUserDto;
             var result = await _userManager.CreateAsync(user, createUserDto.Password);
             return FromIdentityResult(result);
+        }
+
+        public void CsrfCookie()
+        {
+            _antiforgery.SetCookieTokenAndHeader(HttpContext);
+        }
+
+        public AntiforgeryTokenSet CsrfToken()
+        {
+            return _antiforgery.GetTokens(HttpContext);
         }
 
         private IActionResult FromIdentityResult(IdentityResult result)
--- sdlife/src/sdlife.web/wwwroot/app/common/app.ts
+++ sdlife/src/sdlife.web/wwwroot/app/common/app.ts
@@ -39,8 +39,8 @@
 
     app.config(["$httpProvider", ($httpProvider: ng.IHttpProvider) => {
         $httpProvider.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-        let xsrfToken = $("[name=__RequestVerificationToken]").val();
-        $httpProvider.defaults.headers.post["RequestVerificationToken"] = xsrfToken;
-        $httpProvider.interceptors.push("authHttpInterceptor");
+        //let xsrfToken = $("[name=__RequestVerificationToken]").val();
+        //$httpProvider.defaults.headers.post["RequestVerificationToken"] = xsrfToken;
+        //$httpProvider.interceptors.push("authHttpInterceptor");
     }]);
 }
