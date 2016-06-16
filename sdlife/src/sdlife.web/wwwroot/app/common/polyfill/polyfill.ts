13c6c4415b2d8957a6c7cdb4791aa9b6a4168def
Flysha <sdflysha@qq.com>
2016-06-16 11:36:10 +0800

    use custom polyfill instead of es6-shim

---
+++ sdlife/src/sdlife.web/wwwroot/app/common/polyfill/polyfill.ts
@@ -0,0 +1,10 @@
+﻿if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
+
+interface String {
+    startsWith(searchString: string, position?: number): boolean;
+}
