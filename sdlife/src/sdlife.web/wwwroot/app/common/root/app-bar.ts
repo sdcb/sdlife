e29fd1828b4a431266e9e565a223342e5c040b47
flysha <flysha@live.com>
2016-07-02 11:13:54 +0800

    add app-bar to structure

---
+++ sdlife/src/sdlife.web/wwwroot/app/common/root/app-bar.ts
@@ -0,0 +1,41 @@
+﻿namespace sdlife {
+    class AppBarComponent {
+        $router: ng.Router;
+        loading: ng.IPromise<any>;
+
+        showChangePasswordDialog(ev: MouseEvent) {
+            login.showChangePasswordDialog(this.dialog, this.media, ev);
+        }
+
+        logout(ev: MouseEvent) {
+            ensure(this.dialog, ev, "确定要注销登陆吗？").then(() => {
+                return this.loading = this.loginApi.logout();
+            }).then(() => {
+                this.$router.navigate(["Login"]);
+            });
+        }
+
+        openMenu() {
+            this.sidenav("left").toggle();
+        }
+
+        static $inject = ["$mdDialog", "$mdMedia", "login.api", "$mdSidenav"];
+        constructor(
+            public dialog: ng.material.IDialogService,
+            public media: ng.material.IMedia,
+            public loginApi: login.LoginApi,
+            public sidenav: ng.material.ISidenavService) {
+        }
+    }
+
+    let app = angular.module(consts.moduleName);
+    app.component("appBar", {
+        controller: AppBarComponent, 
+        controllerAs: "vm", 
+        templateUrl: "/app/common/root/app-bar.html", 
+        transclude: true, 
+        bindings: {
+            "$router": "<"
+        }
+    });
+}
--- sdlife/src/sdlife.web/wwwroot/app/common/root/app.ts
+++ sdlife/src/sdlife.web/wwwroot/app/common/root/app.ts
@@ -3,31 +3,6 @@
 
     class AppController {
         $router: ng.Router;
-        loading: ng.IPromise<any>;
-
-        showChangePasswordDialog(ev: MouseEvent) {
-            login.showChangePasswordDialog(this.dialog, this.media, ev);
-        }
-
-        logout(ev: MouseEvent) {
-            ensure(this.dialog, ev, "确定要注销登陆吗？").then(() => {
-                return this.loading = this.loginApi.logout();
-            }).then(() => {
-                this.$router.navigate(["Login"]);
-            });
-        }
-
-        openMenu() {
-            this.sidenav("left").toggle();
-        }
-
-        static $inject = ["$mdDialog", "$mdMedia", "login.api", "$mdSidenav"];
-        constructor(
-            public dialog: ng.material.IDialogService,
-            public media: ng.material.IMedia,
-            public loginApi: login.LoginApi,
-            public sidenav: ng.material.ISidenavService) {
-        }
     }
 
     app.component("sdlifeApp", {
