b8dea706e899a99cddce96f23b0217f0db51bffa
flysha <flysha@live.com>
2016-10-17 07:59:45 +0800

    Diary intial: add diary related models.

---
+++ sdlife/src/sdlife.web/Models/Diary/DiaryContent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sdlife.web.Models.Diary
+{
+    public class DiaryContent
+    {
+        public int DiaryId { get; set; }
+
+        [Required]
+        [MaxLength(4000)]
+        public string Content { get; set; }
+    }
+}
---
+++ sdlife/src/sdlife.web/Models/Diary/DiaryHeader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sdlife.web.Models
+{
+    public class DiaryHeader
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime RecordTime { get; set; }
+
+        public User User { get; set; }
+
+        public ICollection<DiaryFeeling> Feelings { get; set; } = new HashSet<DiaryFeeling>();
+    }
+}
