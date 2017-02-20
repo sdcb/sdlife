13fe2784e614f173fb3cce9e9ae1e9c90a020ee4
sdflysha <sdflysha@qq.com>
2017-02-20 18:03:48 +0800

    add basic works component

--- sdlife/src/sdlife.web/src/app/app-routing.module.ts
+++ sdlife/src/sdlife.web/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FooComponent } from './foo/foo.component';
+import { BarComponent } from './bar/bar.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: "foo", component: FooComponent }, 
+  { path: "bar", component: BarComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
   exports: [RouterModule],
   providers: []
 })
