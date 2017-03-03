d2fdee83e3bb2d69aefdd9b147400bbf0386803c
sdflysha <sdflysha@qq.com>
2017-03-03 17:51:01 +0800

    add Jwt

--- sdlife/src/sdlife.web/src/app/login/login.component.ts
+++ sdlife/src/sdlife.web/src/app/login/login.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'], 
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  ngOnInit(): void {
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  loginDto: LoginDto = {
+    username: "",
+    password: "",
+    rememberMe: true
+  };
+
+  login() {
+    console.log(this.loginDto);
+  }
+}
+
+interface LoginDto {
+  username: string;
+  password: string;
+  rememberMe: boolean;
 }
