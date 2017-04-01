32e5c44c783b699574b7f53e7c1b4eba9a6e15d2
sdflysha <sdflysha@qq.com>
2017-04-01 18:19:42 +0800

    fix sign off

--- src/sdlife.web/src/app/app.module.ts
+++ src/sdlife.web/src/app/app.module.ts
@@ -1,4 +1,5 @@
 ﻿import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -30,6 +31,7 @@ import { PageHeaderComponent } from './components/common/page-header.component';
     AppRoutingModule, 
     MaterialModule.forRoot(), 
     FlexLayoutModule, 
+    BrowserAnimationsModule
   ],
   providers: [AppHttpService, TokenStorageService],
   bootstrap: [AppComponent]
